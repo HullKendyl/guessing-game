e1bc4c90b9639e43bd1d853731b7c4256b1ef409
Kendyl Hull <hullkendyl@gmail.com>
2020-08-04 17:04:14 -0400

    added eventlistener to the reset button

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const colors = ["red", "orange", "yellow", "green", "blue"];
 const cards = document.querySelectorAll(".card");
+const resetButton = document.querySelector(".reset");
+
+resetButton.addEventListener("click", () => {
+    location.reload();
+});
 
 cards.forEach((card) => {
     card.addEventListener("click", () => {
@@ -8,6 +13,10 @@ cards.forEach((card) => {
 });
 
 function getCardColor(card) {
+    let currentColor = card.style.backgroundColor;
+    if (currentColor != "") {
+        return false;
+    }
     let x = Math.round(Math.random()*(colors.length-1));
     let color = colors[x];
     card.style.backgroundColor = color;
