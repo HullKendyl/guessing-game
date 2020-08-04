33b384eb25683a7068342091f4c140b70a3af1ef
Kendyl Hull <hullkendyl@gmail.com>
2020-08-04 16:53:03 -0400

    created an eventlistener for the card click and getCardColor function

--- app.js
+++ app.js
@@ -1,2 +1,14 @@
-document.body.style.backgroundColor = "purple";
+const colors = ["red", "orange", "yellow", "green", "blue"];
+const cards = document.querySelectorAll(".card");
 
+cards.forEach((card) => {
+    card.addEventListener("click", () => {
+        getCardColor(card);
+    });
+});
+
+function getCardColor(card) {
+    let x = Math.round(Math.random()*(colors.length-1));
+    let color = colors[x];
+    card.style.backgroundColor = color;
+}
